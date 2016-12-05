d4b3cb8aa3d27ca6adee4cbc02236c8e259ec762
wantonsolutions <stewbertgrant@gmail.com>
2016-12-05 13:19:31 -0800

    added more app data

--- src/Components/App.js
+++ src/Components/App.js
@@ -14,8 +14,8 @@ class App extends Component {
                         </Navbar.Brand>
                     </Navbar.Header>
                     <Nav>
-                        <NavItem eventKey={1} href="/instance/1">Instance 1</NavItem>
-                        <NavItem eventKey={2} href="/test/1">Test</NavItem>
+                        <NavItem eventKey={1} href="/instance/Put-then-Get">Put then Get</NavItem>
+                        <NavItem eventKey={2} href="/instance/1">Test</NavItem>
                         <NavItem eventKey={3} href="/submit/1">Submit</NavItem>
                         <NavItem eventKey={4} href="/">About</NavItem>
                     </Nav>
